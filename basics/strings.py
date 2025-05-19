3ffbc23e252f062dcf5e63489b8239157c12ff53
kamalaly611 <etopiyaaly611@gmail.com>
2025-05-20 04:34:28 +0500

    methods

--- basics/strings.py
+++ basics/strings.py
@@ -1,3 +1,8 @@
 chai='Masla Chai'
 first_char=chai[0]
 print(first_char)
+#now have to extrac masla from Masla chai
+slice_chai=chai[0:6]
+print(slice_chai)
+print(chai[::-1])
+#-----------------------------------> String Methods<-----------------------------------
