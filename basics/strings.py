a9acf7504a1ea1804570636bb9cf24b852cf6d79
kamalaly611 <etopiyaaly611@gmail.com>
2025-05-20 13:55:05 +0500

    Methods

--- basics/strings.py
+++ basics/strings.py
@@ -6,3 +6,30 @@ slice_chai=chai[0:6]
 print(slice_chai)
 print(chai[::-1])
 #-----------------------------------> String Methods<-----------------------------------
+
+str2="hey there"
+print('these is for strip method: ',str2.strip())
+print('these is for Replace method: ',str2.replace('hey','Hi'))
+print('these is for Spilt method: ',str2.split(" "))
+print('these is for Captilize method: ',str2.capitalize())
+print('these is for Center method: ',str2.center(50))
+print('these is for Count method: ',str2.count('e'))
+print('these is for Endswith method: ',str2.endswith('there'))
+print('these is for Find method: ',str2.find('ne'))
+print('these is for Index method: ',str2.index('there'))
+print('these is for Isalnum method: ',str2.isalnum())
+print('these is for isaplpha method: ',str2.isalpha())
+print('these is for Isprintable method: ',str2.isprintable())
+print('these is for Isupper method: ',str2.isupper())
+print('these is for Islower method: ',str2.islower())
+print('these is for Stratwith method: ',str2.startswith('hey'))
+print('these is for Swapcase method: ',str2.swapcase())
+print('these is for title method: ',str2.title())
+print('these is for Join method: ',",".join(str2))
+
+
+
+
+
+
+
