df477b48b8ca58b9de482fcfc0db99619d91dff4
kamalaly611 <etopiyaaly611@gmail.com>
2025-05-22 02:20:04 +0500

    Sets definition

---
+++ basics/sets.py
@@ -0,0 +1,3 @@
+# sets are undordered of data items they store multiple items in a single vairable 
+# sets items are spearted by commas and enclosed within curly bracket 
+# sets are unchangeable sets don't conatins duplicate 
